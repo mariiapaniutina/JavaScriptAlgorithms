02a8282583a8d08ddcee44d9b9556e37faa67ea6
Mariia Paniutina <yangol89@gmail.com>
2015-09-11 09:47:34 -0700

    Update DeleteNodeInLinked List.js

--- js/algorithms/DeleteNodeInLinked List.js
+++ js/algorithms/DeleteNodeInLinked List.js
@@ -6,6 +6,18 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
 the linked list should become 1 -> 2 -> 4 after calling your function. 
  */
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+
 var deleteNode = function(node) {
   if (node.next == null){
     return;
