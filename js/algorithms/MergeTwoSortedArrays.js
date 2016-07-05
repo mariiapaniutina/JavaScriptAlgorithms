3405d97296ab12ec2e6d978708dc42dd40e3d624
Mariia Paniutina <yangol89@gmail.com>
2016-07-04 21:18:22 -0700

    Create MergeTwoSortedArrays.js

---
+++ js/algorithms/MergeTwoSortedArrays.js
@@ -0,0 +1,44 @@
+/*
+Implementation of merging two sorted arrays with extra memory
+*/
+
+/**
+* @params {number []} arr1
+* @params {number []} arr2
+* @returns {number []}
+*/
+var mergeTwoSortedArrays = function(arr1, arr2){
+  var arr1Len = arr1.length || 0;
+  var arr2Len = arr2.length || 0;
+  var totalArrLen = arr1Len + arr2Len;
+  var totalArr = [];
+  var curr1 = 0;
+  var curr2 = 0;
+  var currTotal = 0;
+  var tail;
+
+  while(curr1 < arr1Len && curr2 < arr2Len){    
+    if (arr1[curr1] < arr2[curr2]){
+      totalArr.push(arr1[curr1]);
+      curr1++;
+    } else {
+      totalArr.push(arr2[curr2]);
+      curr2++;
+    }
+    
+    currTotal++;
+  }
+  
+  //add tails
+  if (curr1 < arr1Len) {
+    tail = arr1.splice(curr1);
+    totalArr = totalArr.concat(tail);
+  }
+  
+  if (curr2 < arr2Len) {
+    tail = arr2.splice(curr2);
+    totalArr = totalArr.concat(tail);
+  }
+  
+  return totalArr;
+};
